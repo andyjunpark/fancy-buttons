d99fc9c1d92242c296e74d50756d51a3f7d4c0ef
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-01-17 15:12:52 -0800

    add default state

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -2,11 +2,11 @@ import React from 'react'; //optional
 import { useState } from "react";
 
 function CounterButton(props){
-  const [clickAmount, setClickAmount] = useState();
+  const [clickAmount, setClickAmount] = useState(0);
 
   return (
     <button className="CounterButton">
-      You clicked me X amount of times
+      You clicked me {clickAmount} times
     </button>
   );
 }
